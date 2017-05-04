64eab3889d90ff77a011f49fb54e87bffbe26878
TannerGruebele <tanner@saambaa.com>
2017-05-04 15:43:06 -0700

    added click handlers for characters, started attack logic, bootstrap, added styling, reformed classes and naming conventions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,5 @@
 // The Triwizard Tournement
 
-
-// Make Characters
-	// create function that runs to build characters
-	// function that loops to create the characters placing them in charContainer, then add event listener
-// When user selects character
-	// delete remaining characters and move them to next area
-		// reassinging location and running create, then attaching new event listener
-// User then selects opponent to fight, which gets moved to battle ground
-	// Delete char chosen and move to battleground
-		// reassinging location and running create, (then attaching new event listener on attak button)
-
 // User then "attacks" opponent: User HP increases with every move
 //while opponents attack remains the same
 // If User character HP reaches 0, game over. If opponents HP reaches 0, opponent loses
@@ -20,14 +9,21 @@
 
 window.onload = function() {
 
+char = {};
+wizDef = {};
+heroSelected = false;
+defenderSelected = false;
+enemiesDefeated = 0;
+gameOver = false;
+
 var characters = {
 		harry : {
 			blob: 'harry',
 			name: 'Harry Potter',
 			avatar: '../week-4-game/assets/images/harry.jpg',
 			hp: 150,
-			attack: '',
-			counterAttack: '',
+			attack: 20,
+			counterAttack: 18,
 			location: '.charContainer',
 		},
 		fleur : {
@@ -35,8 +31,8 @@ var characters = {
 			name: 'Fleur Delacour',
 			avatar: '../week-4-game/assets/images/fleur.jpg',
 			hp: 90,
-			attack: '',
-			counterAttack: '',
+			attack: 25,
+			counterAttack: 25,
 			location: '.charContainer',
 		},
 		cedric : {
@@ -44,8 +40,8 @@ var characters = {
 			name: 'Cedric Diggory',
 			avatar: '../week-4-game/assets/images/cedric.png',
 			hp: 110,
-			attack: '',
-			counterAttack: '',
+			attack: 20,
+			counterAttack: 30,
 			location: '.charContainer',
 		},
 		krum : {
@@ -53,42 +49,165 @@ var characters = {
 			name: 'Victor Krum',
 			avatar: '../week-4-game/assets/images/victor.jpg',
 			hp: 200,
-			attack: '',
-			counterAttack: '',
+			attack: 20,
+			counterAttack: 15,
 			location: '.charContainer',
 		},
-
 };
 
-var list = [ characters.harry, characters.fleur, characters.cedric, characters.krum]
-
-
-$.each( list, function( index, char){
-	create(list[index])
-
+$.each( characters, function( index, char){
+	create(characters[index])
 })
 
 function create(player) {
-	$(player.location).append($('<div>').addClass('start style player' + player.blob));
-	$('.player' + player.blob).append($('<p>').html(player.name));
-	$('.player' + player.blob).append($('<img>').attr('src', player.avatar));
-	$('.player' + player.blob).append($('<p>').html(player.hp + 'HP'));
-
-
+	// $('.charsAvailable').append($('<div>').addClass('frames available ' + player.blob));
+	$('.charsAvailable').append($('<div>').addClass('frames').attr('id', player.blob));
+	$('#' + player.blob).append($('<p>').html(player.name));
+	$('#' + player.blob).append($('<img>').attr('src', player.avatar));
+	$('#' + player.blob).append($('<p>').addClass('hp').html(player.hp + 'HP'));
 }
 
-$('.start').on('click', function (){
-	alert("clicked!");
 
-	list.splice(0, 1);
+function setChar(hero) {
+  char.name = hero.name;
+  char.hp = hero.hp;
+  char.attack = hero.attack;
+  char.counterAttack = hero.counterAttack;
 
-	console.log(list);
-})
+  console.log(char);
+}
+
+function setEmeny(villain) {
+  wizDef.name = villain.name;
+  wizDef.hp = villain.hp;
+  wizDef.attack = villain.attack;
+  wizDef.counterAttack = villain.counterAttack;
+
+  console.log(wizDef);
+}
+
+function moveEnemies() {
+	$('.enemies').detach();
+	$('.charsAvailable').removeClass('charactersAvailable').addClass('enemies');
+	$('.defenders').append($('.enemies'));
+}
+
+// Harry Click Handler
+	$('#' + characters.harry.blob).on('click', function (){
+		// console.log(this);
+
+		if(heroSelected === false) {
+			$(this).detach();
+			$(this).addClass('heroChar');
+			$('.myChar').append(this);
+
+			setChar(characters.harry);
+			heroSelected = true;
+			moveEnemies();
+			$(this).unbind();
+
+		} else if ((heroSelected === true) && (defenderSelected !== true)) {
+			$(this).detach();
+			$(this).removeClass('enemies').addClass('defender');
+			$('.battleGround').append(this);
+
+			setEmeny(characters.harry);
+			defenderSelected = true;
+			$(this).unbind();
+		}
+	})
+
+	// Fleur Click Handler
+	$('#' + characters.fleur.blob).on('click', function (){
+		// console.log(this);
+
+		if(heroSelected === false) {
+			$(this).detach();
+			$(this).addClass('heroChar');
+			$('.myChar').append(this);
+
+			setChar(characters.fleur);
+			heroSelected = true;
+			moveEnemies();
+			$(this).unbind();
+
+		} else if ((heroSelected === true) && (defenderSelected !== true)) {
+			$(this).detach();
+			$(this).removeClass('enemies').addClass('defender');
+			$('.battleGround').append(this);
 
 
+			setEmeny(characters.fleur);
+			defenderSelected = true;
+			$(this).unbind();
+		}
+	})
 
+	// Cedric Click Handler
+	$('#' + characters.cedric.blob).on('click', function (){
+		// console.log(this);
 
-console.log(characters);
+		if(heroSelected === false) {
+			$(this).detach();
+			$(this).addClass('heroChar');
+			$('.myChar').append(this);
+
+			setChar(characters.cedric);
+			heroSelected = true;
+			moveEnemies();
+			$(this).unbind();
+
+		} else if ((heroSelected === true) && (defenderSelected !== true)) {
+			$(this).detach();
+			$(this).removeClass('enemies').addClass('defender');
+			$('.battleGround').append(this);
+
+			setEmeny(characters.cedric);
+			defenderSelected = true;
+			$(this).unbind();
+		}
+	})
+
+	// Krum Click Handler
+	$('#' + characters.krum.blob).on('click', function (){
+		// console.log(this);
+
+		if(heroSelected === false) {
+			$(this).detach();
+			$(this).addClass('heroChar');
+			$('.myChar').append(this);
+
+			setChar(characters.krum)
+			heroSelected = true;
+			moveEnemies();
+			$(this).unbind();
+
+		} else if ((heroSelected === true) && (defenderSelected !== true)) {
+			$(this).detach();
+			$(this).removeClass('enemies').addClass('defender');
+			$('.battleGround').append(this);
+
+			setEmeny(characters.krum);
+			defenderSelected = true;
+			$(this).unbind();
+		}
+	})
+
+	$('.attack').on('click', function(){
+		if ((heroSelected === true) && (defenderSelected === true) && (gameOver !== true)) {
+
+		wizDef.hp = wizDef.hp - char.attack;
+		char.attack = char.attack + 8;
+		
+		$('.defender').children('.hp').html(wizDef.hp + ' HP');
+		
+			if(wizDef.hp > 0) {
+				char.hp = char.hp - wizDef.attack;
+				$('.heroChar').children('.hp').html(char.hp + ' HP');
+			}
+		}
+	})
+
 
 }
 
